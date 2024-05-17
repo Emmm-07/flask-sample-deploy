2ebf3c9ce4b6ccfff320b19e2e7f64484e9affa3
Emmm-07 <125170293+Emmm-07@users.noreply.github.com>
2024-05-17 22:24:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+
+
+# Example route to perform an operation
+@app.route('/perform_operation', methods=['POST'])
+def perform_operation():
+    # Extract data from the request
+    data = request.json
+    # Perform the operation (example operation: addition)
+    result = data['num1'] + data['num2']
+    return jsonify({'result': result})
 
 
 if __name__ == '__main__':
